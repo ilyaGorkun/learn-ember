88dadf0cf9ffc113d67e901d6d1bc916faabd76f
Илья Горкун <boricos245@gmail.com>
2020-12-29 17:39:07 +0600

    Change user to users

--- frontend/app/routes/application.js
+++ frontend/app/routes/application.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function () {
-        return this.get('store').findRecord('user', 1);
-    }
+    // model: function () {
+    //     return this.get('store').findRecord('users', 1);
+    // }
 });
