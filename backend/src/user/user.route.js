0645b1f94b61b070ee65b25994522aeccc5c61e0
Илья Горкун <boricos245@gmail.com>
2021-01-02 20:45:31 +0600

    Add a new query parameter

--- backend/src/user/user.route.js
+++ backend/src/user/user.route.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const userController = require('./user.controller');
 
 router.get('/', async (req, res) => {
-  const usersDto = await userController.getUsers(req.query.page);
+  const usersDto = await userController.getUsers(
+    req.query.page,
+    req.query.limit
+  );
   res.status(200).send(usersDto);
 });
 
