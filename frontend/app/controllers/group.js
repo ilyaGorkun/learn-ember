692a63b397518ce3dd970b346740f052c2635d03
Илья Горкун <boricos245@gmail.com>
2021-01-18 12:01:50 +0600

    Union values and users

--- frontend/app/controllers/group.js
+++ frontend/app/controllers/group.js
@@ -4,16 +4,17 @@ export default Ember.Controller.extend({
     name: Ember.computed('group', function () {
         return this.get('group').get('name');
     }),
-    users: Ember.computed('group', function () {
-        const users = this.get('group').get('users');
-        return users;
+    statistic: Ember.computed('values', 'group', function () {
+        const result = this.get('group').get('users').map((user) => {
+            const value = this.get('values').find((value) => {
+                return value.get('userId') === user.id
+            })
+            return {
+                id: value.get('id'),
+                user,
+                value: value.get('value'),
+            }
+        })
+        return result;
     }),
-    values: Ember.computed('values', 'group', function () {
-        return this.get('values');
-    }),
-    actions: {
-        hello() {
-            return "hello"
-        }
-    }
 });
